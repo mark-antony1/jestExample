1fddf4ac83851af058d8a03b340bd37b528cf0bb
jeff christian <jchri@MacBook-Pro-2.loca>
2017-02-04 18:06:16 -0800

    finish basic jest tests

---
+++ src/Link.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from './Link.js';
+import renderer from 'react-test-renderer';
+
+test('Link changes the class when hovered', () => {
+	const component = renderer.create(
+		<Link page='http://www.github.com'>Github</Link>
+	);
+	let tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+
+	tree.props.onMouseEnter();
+	tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+
+	tree.props.onMouseLeave();
+	tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+})
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import Link from './Link.react';
 
 ReactDOM.render(
-  <App />,
+  <Link />,
   document.getElementById('root')
 );
